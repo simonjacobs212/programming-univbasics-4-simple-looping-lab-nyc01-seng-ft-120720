22469144b71d8bfdac7df7d74089770688161c2d
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-16 20:27:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,7 +9,11 @@ def loop_message_n_times(message, n)
   end
 end
 
-def output_array
+def output_array(array)
+  counter = 0
+  while array[counter]
+    counter += 1
+  end
 end
 
 def return_string_array
